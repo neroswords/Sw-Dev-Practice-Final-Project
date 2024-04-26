47b0f3024cc984d6e812df05c5d7a3b0c45e3529
Neroswords <tanathep.min@gmail.com>
2024-04-26 21:45:57 +0700

    cannot update before 3 days from reservation

--- controllers/reservation.js
+++ controllers/reservation.js
@@ -96,6 +96,13 @@ exports.updateReservation = async (req, res, next) => {
         if(reservation.user.toString() !== req.user.id && req.user.role !== 'admin'){
             return res.status(401).json({success: false ,message: `User ${req.user.id} is not authorized to update this reservation`});
         }
+
+        var now = new Date();
+        const nextThreeDays = new Date(now.setDate(now.getDate() + 3));
+        if (reservation.bookingDate <= nextThreeDays){
+            return res.status(401).json({success: false ,message: "can only cancel reservation 3 days before reservation date"});
+        }
+
         reservation = await Reservation.findByIdAndUpdate(req.params.id, {bookingDate, hotel}, {
             new : true,
             runValidators: true
