283c45004c1fe9d2ede82fb68077fa67b85a7c85
Neroswords <tanathep.min@gmail.com>
2024-04-26 22:32:51 +0700

    fix sequence

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,7 +40,7 @@ exports.register = async (req, res, next) => {
 
         sendTokenResponse(user,200,res);
     } catch (err) {
-        res.status(400).json({success:false});
+        res.status(400).json({success:false, msg:err.message});
         console.error(err.stack);
     }
 }
